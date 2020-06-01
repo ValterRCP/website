d7d177ad964e2a6530fa3b64d627060fd0ce5b41
unknown <val26121996@hotmail.com>
2020-06-01 20:36:07 +0200

    Début header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+// Components
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+
+// NgModule
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +22,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
