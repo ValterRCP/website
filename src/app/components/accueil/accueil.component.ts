7e1696459f6ffb8490da590764e8b4857c080b29
unknown <val26121996@hotmail.com>
2020-07-05 22:55:56 +0200

    Section 1 terminée

--- src/app/components/accueil/accueil.component.ts
+++ src/app/components/accueil/accueil.component.ts
@@ -22,34 +22,48 @@ export class AccueilComponent implements OnInit {
 	// Def du tracé contour
 	ctx_1.strokeStyle = "rgb(125,125,125)";
 	ctx_1.lineWidth = 1.5;
-	// Def du chemin contour
+	// Def du chemin contour pour l'animation
 	var vertices=[];
-	vertices.push({x:15, y:0, taille:0});
-	vertices.push({x:canvas_1.width-15-50, y:0, taille:30});
-	vertices.push({x:canvas_1.width-15, y:0+50, taille:5});
-	vertices.push({x:canvas_1.width-15, y:canvas_1.height, taille:15});
-	vertices.push({x:15+50, y:canvas_1.height, taille:30});
-	vertices.push({x:15, y:canvas_1.height-50, taille:5});
-	vertices.push({x:15, y:0, taille:15});
+	vertices.push({x:5, y:5, taille:0});
+	vertices.push({x:canvas_1.width-50, y:5, taille:30});
+	vertices.push({x:canvas_1.width-5, y:0+50, taille:5});
+	vertices.push({x:canvas_1.width-5, y:canvas_1.height-5, taille:15});
+	vertices.push({x:50, y:canvas_1.height-5, taille:30});
+	vertices.push({x:5, y:canvas_1.height-50, taille:5});
+	vertices.push({x:5, y:5, taille:15});
 	// Récupération des points du tracé (défini dans utility.service) contour
 	var points=this.util.calcWaypoints(vertices);
-	// Création du compteur de frames
-	var frameElapsed=1;
 	// Démarrage de l'animation (défini dans utility.service) contour
-	this.util.animate(1,points,ctx_1);
+	this.util.animateLine(1,points,ctx_1);
+	// Def du chemin contour pour gérer le resize
+	window.addEventListener('resize', () => {
+		ctx_1.canvas.width = ctx_1.canvas.clientWidth;
+    	ctx_1.canvas.height = ctx_1.canvas.clientHeight;
+    	ctx_1.strokeStyle = "rgb(125,125,125)";
+		ctx_1.lineWidth = 1.5;
+		var vertices=[];
+		vertices.push({x:5, y:5, taille:0});
+		vertices.push({x:canvas_1.width-50, y:5, taille:30});
+		vertices.push({x:canvas_1.width-5, y:0+50, taille:5});
+		vertices.push({x:canvas_1.width-5, y:canvas_1.height-5, taille:15});
+		vertices.push({x:50, y:canvas_1.height-5, taille:30});
+		vertices.push({x:5, y:canvas_1.height-50, taille:5});
+		vertices.push({x:5, y:5, taille:15});
+		this.util.drawLine(vertices,ctx_1);
+	})
+	
 
-	// Def du canevas cercle
+	// Def du canevas cercle 
 	var canvas_2 = document.getElementById("canvas-2") as HTMLCanvasElement;
-    canvas_2.width = canvas_2.clientWidth;
-	canvas_2.height = canvas_2.clientHeight;
+    canvas_2.width = 480;
+	canvas_2.height = 480;
+	canvas_2.getContext("2d").scale(2,2);
 	var ctx_2 = canvas_2.getContext("2d");
-	// Def du tracé cercle
+	// Def du tracé cercle animé
 	ctx_2.strokeStyle = "rgb(100,100,100)";
-	ctx_2.lineWidth = 1.2;
-	// Def du chemin cercle
-	ctx_2.beginPath();
-	ctx_2.arc(120, 120, 117, 0, 2 * Math.PI);
-	ctx_2.stroke();
+	ctx_2.lineWidth = 2.5;
+	// Démarrage de l'animation (défini dans utility.service) contour
+	this.util.animateCircle(1,ctx_2,120,120,117,50);
   }
 
 }
--- src/app/services/utility.service.ts
+++ src/app/services/utility.service.ts
@@ -24,20 +24,68 @@ export class UtilityService {
 	}
 
 
-	// Fonction permettant d'utiliser les points calculés par clacWaypoints pour effectuer les tracés
+	// Fonction permettant d'utiliser les points calculés par clacWaypoints pour effectuer les animations des tracés linéaires
 	// On récupères les waypoints, le canevas et la frame à laquelle nous sommes pour pouvoir effectuer la récursivité
-	animate(frameElapsed,points,canevas){
-		// On vérifie si on est arrivé au bout des waypoints
-	    if(frameElapsed<points.length-1){ 
+	animateLine(frameElapsed,points,canevas){
+		try {
+			// On vérifie si on est arrivé au bout des waypoints
+		    if(frameElapsed<points.length-1){ 
+		    	// Si ce n'est pas le cas, on redéclenche la fonction pour la frame suivante
+		    	requestAnimationFrame(()=> {
+		    		this.animateLine(frameElapsed+1,points,canevas);
+				}); 
+			}
+		    // On dessine le tracé pour cette frame
+		    canevas.beginPath();
+		    canevas.moveTo(points[frameElapsed-1].x,points[frameElapsed-1].y);
+		    canevas.lineTo(points[frameElapsed].x,points[frameElapsed].y);
+		    canevas.stroke();
+		    // Si ça resize on provoque une erreur
+		    window.addEventListener('resize', function(){
+		    	frameElapsed=1000000;
+			})
+		    return;
+		}
+		catch (e) {
+			if (e.error == undefined) {
+				return;
+			}
+		}
+	}
+
+
+	// Fonction permettant de dessiner des tracés linéaires par défaut
+	drawLine(vertices,canevas){
+		canevas.beginPath();
+		canevas.moveTo(vertices[0].x,vertices[0].y);
+		for(var i=1;i<vertices.length;i++){
+			canevas.lineTo(vertices[i].x,vertices[i].y);
+		}
+		canevas.stroke();
+	}
+
+
+	// Fonction permettant d'effectuer des animations de tracés circulaires
+	animateCircle(frameElapsed,canevas,x,y,r,taille){
+		if(frameElapsed<taille){ 
 	    	// Si ce n'est pas le cas, on redéclenche la fonction pour la frame suivante
 	    	requestAnimationFrame(()=> {
-	    		this.animate(frameElapsed+1,points,canevas);
+	    		this.animateCircle(frameElapsed+1,canevas,120,120,117,50);
 			}); 
 		}
-	    // On dessine le tracé pour cette frame
+		var delta = (frameElapsed*(2 * Math.PI))/400;
+		// On dessine le tracé pour cette frame
 	    canevas.beginPath();
-	    canevas.moveTo(points[frameElapsed-1].x,points[frameElapsed-1].y);
-	    canevas.lineTo(points[frameElapsed].x,points[frameElapsed].y);
+	    canevas.arc(x, y, r, 0-delta, 0+delta);
+	    canevas.stroke();
+	    canevas.beginPath();
+	    canevas.arc(x, y, r, (Math.PI/2)-delta, (Math.PI/2)+delta);
+	    canevas.stroke();
+	    canevas.beginPath();
+	    canevas.arc(x, y, r, Math.PI-delta, Math.PI+delta);
+	    canevas.stroke();
+	    canevas.beginPath();
+	    canevas.arc(x, y, r, (3*Math.PI/2)-delta, (3*Math.PI/2)+delta);
 	    canevas.stroke();
 	}
 
